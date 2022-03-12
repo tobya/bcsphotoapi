788a1f0f3096ff810690a65568e4e21f05e654a2
Toby Allen <toby@toflidium.com>
2022-03-12 09:15:08 +0000

    New Modified version fro laravel

--- routes/web.php
+++ routes/web.php
@@ -1,51 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+Route::get('/', function () {
+    return view('welcome');
 });
-
-/*
-  Get All Galleries or all Galleries for specific Year.
-*/
-$router->get('/all', 'photoController@AllGalleryInfo_ConvertDBPath');
-$router->get('/all/{year}', 'photoController@YearPhotoInfo');
-$router->get('/allconvertzen', 'photoController@AllGalleryInfo_ConvertDBPath');
-$router->get('/allloadrecipepaths', 'photoController@AllGalleryInfo_IncludingPathIDs');
-$router->get('/gallerypathurls', 'photoController@AllGalleryPathURLs');
-
-// Get Random Image
-$router->get('/images/random/', 'photoController@GalleryImageRandom');
-$router->get('/images/random/{year}/', 'photoController@GalleryImageRandomYear');
-$router->get('/images/random/{year}/{month}/', 'photoController@GalleryImageRandomMonth');
-$router->get('/images/random/{year}/{month}/{day}', 'photoController@GalleryImageRandomDay');
-
-
-// Get Specific Gallery info for date.
-$router->get('/gallery/{demodate}', 'photoController@GalleryAlbum');
-$router->get('/gallery/{demodate}/nocache', 'photoController@GalleryAlbum_noCache');
-
-$router->get('/files/all', 'photoController@LoadAllPhotos');
-
-// Return Gallery as basic HTML rather than JSON
-$router->get('/gallery/{demodate}/html/', 'photoController@HTMLGalleryAlbum');
-
-
-$router->get('/gallery/{demodate}/html/{template}', 'templateController@HTMLGalleryAlbum');
-
-
-
-$router->get('/purgecache/', 'photoController@PurgeCache');
-
-
-
