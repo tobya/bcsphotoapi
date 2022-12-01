ae505e2e774b48d1e537dbaf75fcaa7ae8154420
Toby Allen <toby@cookingisfun.ie>
2022-12-01 19:49:12 +0000

    update version

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'version' => '2.1',
+    'version' => '2.2.0',
     /*
     |--------------------------------------------------------------------------
     | Application Environment
