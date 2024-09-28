4ad376ac72467fbee56196e5f0aacfd19c2d6908
Toby Allen <toby@cookingisfun.ie>
2024-09-28 20:18:35 +0100

    list

--- resources/views/gallery/nicelist.blade.php
+++ resources/views/gallery/nicelist.blade.php
@@ -11,13 +11,14 @@
 
 <div class="bg-white">
   <div class="mx-auto max-w-full px-4 lg:py-16  lg:max-w-7xl lg:px-8">
-    <h2 class="sr-only">Products</h2>
+    <h1 class="text-blue-950 text-xl font-bold p-2">Demo Photos for {{$Demo->Folder_HumanReadable}}</h1>
+    <h2 class="sr-only">Demo photos</h2>
 
     <div class="grid grid-cols-1 gap-x-6 gap-y-10  lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           @foreach ($Photos as $P)
       <a href="#" class="group">
         <div class=" w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-          <img src="{{$P->basesrc}}?w=400&h=400&fit=crop"
+          <img src="{{$P->basesrc}}?w=500&h=500&fit=crop"
 
                class="h-full w-full object-cover object-center group-hover:opacity-75">
         </div>
