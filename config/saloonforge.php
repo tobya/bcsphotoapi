e183d9760ea9c6b37af359da66f789de44f6af9a
Toby Allen <toby@cookingisfun.ie>
2026-09-11 22:24:04 +0100

    no need for custom selector

--- config/saloonforge.php
+++ config/saloonforge.php
@@ -3,34 +3,33 @@
 // config for Tobya/SaloonForge
 return [
     'integrations' => [
-        'Photo' => [
+        'Photos2' => [
             'routes' => [
 
-            'selector_class' => \App\Http\Integrations\PhotoApiRouteSelector::class,
+          //  'selector_class' => \App\Http\Integrations\PhotoApiRouteSelector::class,
+            'selector_class' => \Tobya\SaloonForge\Selectors\RouteSelector::class,
             'forgeroute_class' => \Tobya\SaloonForge\Extensions\ForgeRoute::class,
 
-
-            'prefix' => ['/'],
             'exclude' =>
                 [
-                  'middleware' => ['web'],
-                 'filter' =>        ['somenonese*'],
-                 'unnamed' => false,
+                  'middleware'  =>  ['web'],
+                 'filter'       =>  ['somenonese*'],
+                 'unnamed' => true,
                 ],
+
             'include' =>
                 [
-                'filter' =>       ['*'],
-                'middleware' => ['web'],
-                'route-parameters' => [
-                    'any' =>    [],
-                    'all' => [],  // not implemented
-                ],
+                'filter' =>       [],
+
+                'middleware' => ['api'],
+
+
                 ]
         ],
              'namespace' => 'Bcsapi\\V5\\Photo',
           'output' => [
               'dir' => base_path('Modules/bcsapi/V5/'),
-            'copy' => [
+              'copy' => [
                   'active' => true,
                   'destination' => 'C:\\Development\\github\\packages\\bcsapi\\bcsapiwrapper\\src\\V5\\Photo\\',
               ]
